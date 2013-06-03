2dee335903b09cee391f01ee871ae21cb8582414
Adam Brett <adam@adambrett.co.uk>
2013-06-03 21:37:57 +0100

    updated cache @ 2013-06-03 21:37:57

--- deploy.sh
+++ deploy.sh
@@ -1,62 +1,61 @@
 #!/usr/bin/env bash
 
-# executables prefix
-_prefix="/usr/bin"
 # git executable
-_git="$_prefix/git"
+git="git"
 
 # site generation executable
-_generate="bundle exec jekyll"
+generate="jekyll"
 # options for the generator
-_opts=(--no-safe --no-server --no-auto --kramdown)
+opts=(--no-safe --no-server --no-auto --kramdown)
 
 # branch from which to generate site
-_origbranch="master"
+origbranch="master"
+
 # branch holding the generated site
-_destbranch="gh-pages"
+destbranch="gh-pages"
 
 # directory holding the generated site -- should be outside this repo
-_site="$("mktemp" -d /tmp/_site.XXXXXXXXX)"
+site="$("mktemp" -d /tmp/_site.XXXXXXXXX)"
 
 # the current branch
-_currbranch="$(grep "^*" < <("$_git" branch) | $_prefix/cut -d' ' -f2)"
+currbranch="$(grep "^*" < <("$git" branch) | cut -d' ' -f2)"
 
-if [[ $_currbranch == $_origbranch ]]; then # we should generate the site
+if [[ $currbranch == $origbranch ]]; then # we should generate the site
     # go to root dir of the repo
-    cd "$("$_git" rev-parse --show-toplevel)"
+    cd "$("$git" rev-parse --show-toplevel)"
 
     # generate the site
-    "$_generate" ${_opts[@]} . "$_site"
+    "$generate" ${opts[@]} . "$site"
 
     # add any new files
-    "$_git" add .
+    "$git" add .
 
     # commit all changes with a default message
-    "$_git" commit -a -m "updated cache @ $(date +"%F %T")"
+    "$git" commit -a -m "updated cache @ $(date +"%F %T")"
 
     # switch to branch the site will be stored
-    "$_git" checkout "$_destbranch"
+    "$git" checkout "$destbranch"
 
     # overwrite existing files
     builtin shopt -s dotglob
-    cp -rf "$_site"/* .
+    cp -rf "$site"/* .
     builtin shopt -u dotglob
 
     # add any new files
-    "$_git" status
+    "$git" status
 
     # add any new files
-    "$_git" add .
+    "$git" add .
 
     # commit all changes with a default message
-    "$_git" commit -a -m "updated site @ $(date +"%F %T")"
+    "$git" commit -a -m "updated site @ $(date +"%F %T")"
 
     # push changes to github
-    "$_git" push origin "$_destbranch"
+    "$git" push origin "$destbranch"
 
     # cleanup
-    rm -rfv "$_site"
+    rm -rfv "$site"
 
     # return
-    "$_git" checkout "$_origbranch"
+    "$git" checkout "$origbranch"
 fi
