76b72f4b05f777554081a2e747ab2306adaddfd9
Adam Brett <adam@adambrett.co.uk>
2013-07-02 15:59:10 +0100

    updated cache @ 2013-07-02 15:59:10

--- deploy.sh
+++ deploy.sh
@@ -1,40 +1,23 @@
 #!/usr/bin/env bash
 
-# git executable
-git="git"
-
-# site generation executable
-generate="jekyll"
-# options for the generator
-opts=(build)
-
-# branch from which to generate site
-origbranch="master"
-
-# branch holding the generated site
-destbranch="gh-pages"
-
-# directory holding the generated site -- should be outside this repo
-site="$("mktemp" -d /tmp/_site.XXXXXXXXX)"
-
-# the current branch
-currbranch="$(grep "^*" < <("$git" branch) | cut -d' ' -f2)"
-
-if [[ $currbranch == $origbranch ]]; then # we should generate the site
+currbranch="$(grep "^*" < <(git branch) | cut -d' ' -f2)"
+if [[ $currbranch == "master" ]]; then # we should generate the site
     # go to root dir of the repo
-    cd "$("$git" rev-parse --show-toplevel)"
+    cd "$(git rev-parse --show-toplevel)"
+
+    site="$("mktemp" -d /tmp/_site.XXXXXXXXX)"
 
     # generate the site
-    "$generate" ${opts[@]} . "-d $site"
+    jekyll build -d $site
 
     # add any new files
-    "$git" add .
+    git add -A
 
     # commit all changes with a default message
-    "$git" commit -a -m "updated cache @ $(date +"%F %T")"
+    git commit -a -m "updated cache @ $(date +"%F %T")"
 
     # switch to branch the site will be stored
-    "$git" checkout "$destbranch"
+    git checkout gh-pages
 
     # overwrite existing files
     builtin shopt -s dotglob
@@ -42,20 +25,20 @@ if [[ $currbranch == $origbranch ]]; then # we should generate the site
     builtin shopt -u dotglob
 
     # add any new files
-    "$git" status
+    git status
 
     # add any new files
-    "$git" add .
+    git add -A
 
     # commit all changes with a default message
-    "$git" commit -a -m "updated site @ $(date +"%F %T")"
+    git commit -a -m "updated site @ $(date +"%F %T")"
 
     # push changes to github
-    "$git" push origin "$destbranch"
+    git push origin gh-pages
 
     # cleanup
     rm -rfv "$site"
 
     # return
-    "$git" checkout "$origbranch"
+    git checkout master
 fi
